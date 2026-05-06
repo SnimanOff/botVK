70e6295db2e91286f8ed50074937df9b6aabfa01
sidorin.vadim.08@gmail.com <sniman.08@gmail.com>
2026-05-06 21:15:27 +0300

    start get_item

--- src/database/service/user.py
+++ src/database/service/user.py
@@ -1,22 +1,30 @@
 from sqlalchemy import select, insert
 from database.core import get_session
-from database.models import Players
+from database.models import Players, Items
 from loguru import logger
 
 class UserService:
     
     @staticmethod
-    async def create_user(vk_id: int) -> Players:
+    async def GoC_user(vk_id: int) -> Players:
+        """
+        get or create user 
+        
+        Ищет профиль пользователя в базе данных по vk_id
+        При ненаходе создаёт профиль 
+        
+        возвращает модель игрока
+        """
         async with get_session() as session:
             result = await session.execute(
                 select(Players)
                 .where(Players.vk_id == vk_id)
             )
-            existing = result.scalar_one_or_none()
+            player = result.scalar_one_or_none()
             
-            if existing:
-                logger.debug("Игрок vk_id={} уже существует, id={}", existing.vk_id, existing.id)
-                return existing
+            if player:
+                logger.debug("Игрок vk_id={} уже существует, id={}", player.vk_id, player.id)
+                return player
             
             player = Players(
                 vk_id = vk_id,
@@ -28,3 +36,19 @@ class UserService:
             
             logger.info("Создан игрок id={}, vk_id={}", player.id, vk_id)
             return player
+        
+        @staticmethod
+        async def get_item(code: str) -> Items:
+            async with get_session() as session:
+                result = await session.execute(
+                    select(Items)
+                    .where(Items.code == code)
+                )
+                
+                item = result.scalar_one_or_none()
+                
+                if not item:
+                    logger.debug("Запрашиваемый предмет не найден, код искуемого: {}", code)
+                    return item
+                else:
+                    
