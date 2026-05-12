82255bfe04f139a57eecf66b7292c7d7dea3734a
sidorin.vadim.08@gmail.com <sniman.08@gmail.com>
2026-05-12 14:11:08 +0300

    moved files, first run

---
+++ src/main.py
@@ -0,0 +1,37 @@
+import asyncio
+
+from vkbottle.bot import Bot
+from loguru import logger
+
+from settings import settings
+from logger import setup_logger
+from database.run import init_db, close_db
+from bot.handlers.start import labeler as start_labeler
+
+
+bot = Bot(token=settings.VK_TOKEN)
+bot.labeler.load(start_labeler)
+
+
+def main() -> None:
+    setup_logger()
+
+    try:
+        logger.info("Запуск приложения...")
+        asyncio.run(init_db())
+
+        logger.info("Бот запущен")
+        bot.run_forever()
+
+    except KeyboardInterrupt:
+        logger.warning("Бот остановлен вручную")
+
+    except Exception as error:
+        logger.exception("Ошибка при запуске: {}", error)
+
+    finally:
+        asyncio.run(close_db())
+
+
+if __name__ == "__main__":
+    main()
